3791bc01507f50970dc21514ca859557ebd2fbb2
Munawer Anwar <Munaweranwar2@gmail.com>
2025-03-05 22:27:16 +0500

    updated the test result api

--- src/results/dto/create-result.dto.ts
+++ src/results/dto/create-result.dto.ts
@@ -12,14 +12,34 @@ export class CreateResultDto {
   @ApiProperty({ example: '60d0fe4f5311236168a109cc' })
   readonly student: Types.ObjectId;
 
+  @ApiProperty({ example: '60d0fe4f5311236168a109cd' })
+  readonly institute: Types.ObjectId;
+
+  @ApiProperty({ example: 10 })
+  readonly numOfQuestions: number;
+
+  @ApiProperty({ example: 5 })
+  readonly marksPerQuestion: number;
+}
+
+export class CreateResultServiceDto {
+  @ApiProperty({ example: '60d0fe4f5311236168a109ca' })
+  readonly test: Types.ObjectId;
+
+  @ApiProperty({ example: '60d0fe4f5311236168a109cb' })
+  readonly subject: Types.ObjectId;
+
+  @ApiProperty({ example: '60d0fe4f5311236168a109cc' })
+  readonly student: Types.ObjectId;
+
+  @ApiProperty({ example: '60d0fe4f5311236168a109cd' })
+  readonly institute: Types.ObjectId;
+
   @ApiProperty({ example: new Date() })
   readonly startedAt: Date;
 
-  @ApiProperty({ example: new Date() })
-  readonly finishedAt: Date;
-
   @ApiProperty({ example: ResultStatus.FINISHED, enum: ResultStatus })
-  readonly status: ResultStatus;
+  readonly status?: ResultStatus;
 
   @ApiProperty({ example: 10 })
   readonly numOfQuestions: number;
