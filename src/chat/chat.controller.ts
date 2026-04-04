381e6b3db29def4f71f0f9b199fb258be369360f
munawar <Munaweranwar2@gmail.com>
2026-04-04 21:51:38 +0500

    Update chat.controller.ts

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ChatService } from './chat.service';
 
-@Controller('chat')
+@Controller(['chat', 'api/chat'])
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
